810f466f152cda3f1193799244177ec8460823d0
David Rose <david.github@drose.io>
2026-02-05 16:53:03 -0500

    test(e2e): assert thread isolation via data-thread-id

--- apps/zerg/frontend-web/src/pages/ChatPage.tsx
+++ apps/zerg/frontend-web/src/pages/ChatPage.tsx
@@ -353,6 +353,7 @@ export default function ChatPage() {
             streamingMessageId={streamingMessageId}
             pendingTokenBuffer={pendingTokenBuffer}
             onCopyMessage={handleCopyMessage}
+            threadId={effectiveThreadId}
           />
         </div>
 
